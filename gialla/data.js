f37f0e8d91705d52d5450381a81b133cd18d5f9e
z-gestioni <info@z-gestioni.it>
2025-12-16 22:25:35 +0100

    B&B

---
+++ gialla/data.js
@@ -0,0 +1,212 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-letto-matrimoniale",
+      "name": "letto matrimoniale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -2.899553813067339,
+        "pitch": 0.35651465179751796,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9678502473995074,
+          "pitch": 0.5134924259048272,
+          "rotation": 0.7853981633974483,
+          "target": "3-bagno"
+        },
+        {
+          "yaw": -0.4971640184445558,
+          "pitch": 0.47081273945488,
+          "rotation": 0,
+          "target": "1-letto-singolo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-letto-singolo",
+      "name": "letto singolo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.9140382169215417,
+        "pitch": 0.40440736639626884,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.19045654536928502,
+          "pitch": 0.6803157922310916,
+          "rotation": 0,
+          "target": "2-entrata"
+        },
+        {
+          "yaw": -3.035218441736168,
+          "pitch": 0.5743578284326709,
+          "rotation": 5.497787143782138,
+          "target": "0-letto-matrimoniale"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-entrata",
+      "name": "entrata",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.841081387989771,
+        "pitch": 0.12281946577054015,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9462447455634653,
+          "pitch": 0.7898430184519256,
+          "rotation": 0,
+          "target": "1-letto-singolo"
+        },
+        {
+          "yaw": -0.011057986209046078,
+          "pitch": 0.5281895463152804,
+          "rotation": 0,
+          "target": "4-pianerottolo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1218353672616423,
+          "pitch": 0.4136015915711617,
+          "title": "sala colazioni",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-bagno",
+      "name": "bagno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-pianerottolo",
+      "name": "pianerottolo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.504934665130847,
+        "pitch": 0.23089723410164353,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Camera Gialla",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ rossa/data.js
@@ -0,0 +1,269 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-soggiorno-1",
+      "name": "soggiorno 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.06784111193696418,
+        "pitch": 0.376848408132604,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.116302375240947,
+          "pitch": 0.5639049272510483,
+          "rotation": 0,
+          "target": "4-corridoio"
+        },
+        {
+          "yaw": -1.0658542146677128,
+          "pitch": 0.43662633011016894,
+          "rotation": 5.497787143782138,
+          "target": "1-soggiorno-2"
+        },
+        {
+          "yaw": 0.8777557319186453,
+          "pitch": 0.4865596855837637,
+          "rotation": 0.7853981633974483,
+          "target": "2-letto-matrimoniale"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-soggiorno-2",
+      "name": "soggiorno 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.8737230214555076,
+        "pitch": 0.33675025932566527,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8853521734236507,
+          "pitch": 0.5151109222096739,
+          "rotation": 0.7853981633974483,
+          "target": "0-soggiorno-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-letto-matrimoniale",
+      "name": "letto matrimoniale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.2469757374594597,
+        "pitch": 0.22645273643779973,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3752045735691247,
+          "pitch": 0.6742192572539913,
+          "rotation": 0,
+          "target": "0-soggiorno-1"
+        },
+        {
+          "yaw": -1.3808677339666549,
+          "pitch": 0.332601160497596,
+          "rotation": 6.283185307179586,
+          "target": "1-soggiorno-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bagno",
+      "name": "bagno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.5546580083689214,
+        "pitch": 0.41181525860262624,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.064920892010411,
+          "pitch": 0.6876358186829599,
+          "rotation": 0,
+          "target": "4-corridoio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-corridoio",
+      "name": "corridoio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.5775233504634603,
+        "pitch": 0.09793156534935932,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5177828865970069,
+          "pitch": 0.5943618089774905,
+          "rotation": 0,
+          "target": "0-soggiorno-1"
+        },
+        {
+          "yaw": -0.10368018052821348,
+          "pitch": 0.7162373644293734,
+          "rotation": 0,
+          "target": "3-bagno"
+        },
+        {
+          "yaw": 3.1113610483927765,
+          "pitch": 0.7368627884639096,
+          "rotation": 0,
+          "target": "5-pianerottolo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pianerottolo",
+      "name": "pianerottolo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -2.49699858915033,
+        "pitch": 0.20221777689012477,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7319569945869357,
+          "pitch": 0.6003008810332542,
+          "rotation": 0,
+          "target": "4-corridoio"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Camera Rossa",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
